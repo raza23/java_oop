f35c17afeda6327986bff2c2966782c23d591d13
Raza <rtshareef@gmail.com>
2021-07-06 19:38:21 -0400

    added object to file tree

---
+++ src/Obj.java
@@ -0,0 +1,17 @@
+public class Obj {
+//    PEN OBJECT
+    String type = "gel";
+    String color = "blue";
+    int point = 10;
+
+    static boolean clicked = false;
+
+    public static void click() {
+        clicked = true;
+    }
+
+    public static voic unclicked(){
+        clicked = false;
+    }
+
+}
